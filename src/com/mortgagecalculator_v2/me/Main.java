6082e59148188d63dfa002ae6b752806c4cebe5a
Hashinur Rashid Sohan <mhashinurrashidsohan199@gmail.com>
2025-07-25 11:04:29 +0600

    done

--- src/com/mortgagecalculator_v2/me/Main.java
+++ src/com/mortgagecalculator_v2/me/Main.java
@@ -27,7 +27,7 @@ public class Main {
         }
 
         while (true) {
-            System.out.print(" Annual Interest Rate");
+            System.out.print(" Annual Interest Rate: ");
             float annualInterest = scanner.nextFloat();
             if(annualInterest >= 1 && annualInterest <= 30) {
                  monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
