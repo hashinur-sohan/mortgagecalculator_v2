76a034c0502033726541659495eac96ee7142c2c
Hashinur Rashid Sohan <mhashinurrashidsohan199@gmail.com>
2025-07-25 09:49:53 +0600

    my first commit

---
+++ src/com/mortgagecalculator_v2/me/Main.java
@@ -0,0 +1,20 @@
+package com.mortgagecalculator_v2.me;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: " );
+        int principal = scanner.nextInt();
+
+        System.out.print(" Annual Interest Rate");
+
+    }
+
+}
