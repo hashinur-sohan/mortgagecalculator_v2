d3e56d480e7b3efa4d5ec276eb54dbd23d731342
Hashinur Rashid Sohan <mhashinurrashidsohan199@gmail.com>
2025-07-25 10:35:29 +0600

    Principal calculation part done

--- src/com/mortgagecalculator_v2/me/Main.java
+++ src/com/mortgagecalculator_v2/me/Main.java
@@ -1,6 +1,8 @@
 package com.mortgagecalculator_v2.me;
 
 
+import com.sun.source.tree.WhileLoopTree;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,12 +10,21 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float annualInterest = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: " );
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.print("Enter a value between 1000 to 1000000 ");
 
-        System.out.print(" Annual Interest Rate");
+
+
+        }
 
     }
 
